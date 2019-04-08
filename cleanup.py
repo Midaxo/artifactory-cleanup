7ae4a11006f6aef5b50beaec69bcbed93cbf4377
Juha Luoto <juha.luoto@eficode.com>
2019-04-08 09:40:34 +0300

    Clean only certain repos

--- cleanup.py
+++ cleanup.py
@@ -9,43 +9,53 @@ username = os.environ['ARTIFACTORY_USERNAME']
 password = os.environ['ARTIFACTORY_PASSWORD']
 credentials = base64.b64encode(username + ":" + password)
 headers = {'Authorization': 'Basic ' + credentials}
+artifactAgeInDays = 150
+
+cleanupRepos = [
+    'Midaxo.Auth',
+    'account-settings',
+    'admin-console',
+    'apigateway',
+    'auth',
+    'exago',
+    'exago-api-loader',
+    'exago-integration-service',
+    'exago-web',
+    'frontend',
+    'platform',
+    'sharepoint'
+]
 
 def main():
-    deleteOldArtifactsFromAllRepos()
-
-def deleteOldArtifactsFromAllRepos():
-    for repo in getLocalRepos():
-        deleteOldArtifacts(repo['key'])
-
-def getLocalRepos():
-    params = {'type': 'LOCAL'}
-    r = requests.get(url = url + "/api/repositories", headers = headers, params = params)
-    return json.loads(r.content)
+    for repo in cleanupRepos:
+        deleteOldArtifacts(repo)
 
 def deleteOldArtifacts(repo):
-    print "Finding removable artifacts for repository %s..." % repo
-    criteria = createSearchCriteria(repo = repo, maxAgeInDays = 5)
-    print criteria
+    criteria = createSearchCriteria(repo = repo)
     results = findArtifacts(criteria)
-    print 'Found %s artifacts for %s ' % (results['range']['total'], repo)
-
+    print 'Found %s removable artifacts for %s ' % (results['range']['total'], repo)
+    for artifact in results['results']:
+        print artifact['name']
+    
 def findArtifacts(criteria):
     query = "items.find(%s)" % criteria
-    #query = 'items.find({"repo": "%s", "@skipCleanup": {"$ne": "true"}})' % repo
     r = requests.post(url = url + "/api/search/aql", headers = headers, data = query)
     return json.loads(r.content)
 
-def createSearchCriteria(repo, maxAgeInDays):
+def createSearchCriteria(repo):
     criteria = {}
     # Only search the given Artifactory repository
     criteria['repo'] = repo
-    # Get artifacts created within maxAgeInDays
-    criteria['created'] = {"$gt" : getIsoDate(maxAgeInDays)}
-    # Skip any artifact that has the label skipCleanup set to 'true'
+    # Get artifacts created within artifactAgeInDays
+    criteria['created'] = {"$lt" : getDateDaysAgo(artifactAgeInDays)}
+    # Only include artifacts that DO NOT have skipCleanup set to 'true'
     criteria['@skipCleanup'] = {"$ne": "true"}
+    # Do not include release artifacts
+    criteria['name'] = {"$nmatch":"release-*"}
+
     return json.dumps(criteria)
 
-def getIsoDate(daysAgo):
+def getDateDaysAgo(daysAgo):
     d = date.today() - timedelta(days = daysAgo)
     return d.isoformat()
 
